021aaf47333896c03fd377450a1f735efcc13e2f
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-10-13 12:30:48 -0400

    Write Tensor.py (create basic tensor cnn model)

--- Tensor/DataProcessor.py
+++ Tensor/DataProcessor.py
@@ -1,4 +1,4 @@
-from Tensor.tensor_imports import *
+from tensor_imports import *
 
 # Convert image into float matrix
 def convert_to_float(image, label):
@@ -10,16 +10,18 @@ class DataProcessor:
     # MARK: Initializer
 
     def __init__(self):
-
+        self.dataset = None
         self.train, self.cv, self.test = None, None, None
+        self.img_w, self.img_h = 256, 256
+        self.batch_size = 32
 
         dataset = image_dataset_from_directory(
-            './train',
+            './data/train',
             labels='inferred', # Get your own output labels from .txt files
             label_mode='binary',
-            image_size=[256,256], # This will change later
+            image_size=[self.img_w,self.img_h], # This will change later
             interpolation='nearest', # Method used when resizing the images
-            batch_size=32, # Training batch for images (should match the batch_size in model.fit)
+            batch_size=self.batch_size, # Training batch for images (should match the batch_size in model.fit)
             shuffle=True # Shuffle data
         )
 
@@ -28,9 +30,14 @@ class DataProcessor:
         dataset = (dataset.map(convert_to_float).cache().prefetch(AUTOTUNE))
         # Last two batches are used for cross-validation and test
         train, cv, test = dataset.take(len(dataset) - 2), dataset.skip(len(dataset) - 2).take(1), dataset.skip(len(dataset) - 1).take(1)
+        # Assign dataset to attributes
+        self.dataset = dataset
         self.train, self.cv, self.test = train, cv, test
     
     # MARK: Getters
+    def get_all(self) -> tf.data.Dataset:
+        return self.dataset
+    
     def get_train(self, augment=False) -> tf.data.Dataset: 
         if augment:
             # Make a sequential model to augment data
--- Tensor/TensorModel.py
+++ Tensor/TensorModel.py
@@ -1,9 +1,7 @@
-from Tensor.tensor_imports import *
-import Tensor.DataProcessor as DataProcessor
+from tensor_imports import *
+from DataProcessor import DataProcessor
 
 class TensorModel:
-
-    model_data: DataProcessor = DataProcessor()
 
     def __init__(self) -> None:
         # Set logger for debug
@@ -15,5 +13,45 @@ class TensorModel:
         np.random.seed(12345)
         tf.random.set_seed(12345)
         os.environ["PYTHONHASHSEED"] = str(SEED)
-        # Get full image dataset
+        # Set initial model to None
+        self.model = None
+        self.model_data: DataProcessor = DataProcessor()
+    
+    def build_model(self):
+        m = Sequential()
+        # Format Train Data
+        m.add(InputLayer(shape=[self.model_data.img_w,self.model_data.img_h,3]))
+        m.add(BatchNormalization())
+        # Convolutional Block
+        m.add(Conv2D(filters=3,kernel_size=(3,3),dilation_rate=(1,1),padding='valid',activation='relu'))
+        m.add(MaxPool2D()) # def. pool size is 2x2
+        m.add(Flatten())
+        # Inference Block
+        m.add(Dense(units=128,activation='relu'))
+        m.add(Dense(units=128,activation='relu'))
+        m.add(Dense(units=64,activation='relu'))
+        m.add(Dense(units=16,activation='relu'))
+        m.add(Dense(units=1,activation='sigmoid',kernel_regularizer=L2(0.005)))
+        self.model = m
+    
+    def train_model(self):
+        if self.model == None:
+            return
         
+        self.model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+        history = self.model.fit(self.model_data.get_train(),validation_data=self.model_data.get_cv(),batch_size=self.model_data.batch_size,epochs=50,verbose=1)
+        print(type(history))
+        return history
+
+    def test_model(self):
+        if self.model == None:
+            return
+        
+        results = self.model.evaluate(self.model_data.get_test(),verbose=1)
+        return results
+
+
+model = TensorModel()
+model.build_model()
+model.train_model() 
+model.test_model()     
