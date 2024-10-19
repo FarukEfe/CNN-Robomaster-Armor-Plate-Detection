1b8497a6777c14edc463b313309e83fe7eb7d4c2
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-10-18 22:47:29 -0400

    Write ModelTester.py to make reliable consensus on the best model

--- Tensor/ModelTester.py
+++ Tensor/ModelTester.py
@@ -6,12 +6,6 @@ class ModelTester:
 
     models = {}
 
-    best_model = {
-        'model': None,
-        'loss': -1,
-        'acc': -1
-    }
-
     def __init__(self):
         n = os.listdir("Tensor/best_models")
         for i in n:
@@ -22,9 +16,6 @@ class ModelTester:
     def show_models(self):
         keys = self.models.keys()
         print(keys)
-    
-    def show_best(self):
-        print(self.best_model)
 
     # Get the AVG of test results
     def __test_model(self, model_save: str, rep: int) -> float:
@@ -45,26 +36,39 @@ class ModelTester:
         best_acc, best_loss, best_key = -1, -1, None
         for key in keys:
             acc, loss = self.__test_model(key,rep)
-            print(f"{key} has accuracy {acc} and loss {loss}")
+            #print(f"{key} has accuracy {acc} and loss {loss}")
             best_key = key if acc > best_acc else best_key
             best_loss = loss if acc > best_acc else best_loss
             # Ensure best_acc updates last since the above values depend on acc > best_acc comparisons
             best_acc = acc if acc > best_acc else best_acc
-        self.best_model['model'] = self.models[key]
-        self.best_model['acc'] = best_acc
-        self.best_model['loss'] = best_loss
+
+        best_model = {
+            'key': key,
+            'model': self.models[key],
+            'acc': best_acc,
+            'loss': best_loss
+        }
+        return best_model
     
-    def save_best(self):
+    def consensus(self, test_rep: int, repeat_rep: int):
+        bms = []
+        for _ in range(repeat_rep):
+            bm = self.cherry_pick(test_rep)
+            print(bm)
+            bms.append(bm)
+        return bms
+    
+    def save_best(self, best_model):
         # If no model is selected, return without saving
-        if self.best_model['model'] == None:
+        if best_model['model'] == None:
             print("No model selected, aborting from operation ...")
             return
         # Get best model and its stats
-        best_pick: TensorModel = self.best_model['model']
-        best_stats: dict = { 'acc': self.best_model['acc'], 'loss': self.best_model['loss'] }
+        best_pick: TensorModel = best_model['model']
+        best_stats: dict = { 'acc': best_model['acc'], 'loss': best_model['loss'] }
         # Display best model stats
         print(best_stats)
-        _ = input("If you want to abando, ctrl + c.\n")
+        _ = input("If you want to abandon, ctrl + c.\n")
         # Save best model
         best_pick.final_train(epochs=125)
         best_pick.save_model(final=True)
@@ -72,6 +76,5 @@ class ModelTester:
 if __name__ == "__main__":
     tester = ModelTester()
     tester.show_models()
-    tester.cherry_pick(5)
-    tester.show_best()
-    #tester.save_best()
+    dec_list = tester.consensus(10, 10)
+    #tester.save_best(bm)
