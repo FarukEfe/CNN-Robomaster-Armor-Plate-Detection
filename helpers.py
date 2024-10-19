3c92f95764b6d9911df825f17448dd0519f36b38
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-10-19 11:55:15 -0400

    Write helpers.py (minor fixes for consistency)

--- helpers.py
+++ helpers.py
@@ -2,28 +2,17 @@ import os
 import shutil
 from math import floor
 
-def order_dataset(val_split: float = 0.2, test_split: float = 0.1):
-    if val_split + test_split >= 1:
-        print("Validation + Test cannot exceed 1")
-        return
-    
+def order_dataset():    
     if (os.path.exists("./train/pos")):
         return
+    # Create directories
+    moves: list[tuple[str]] = []
+    removes = []
     # Create new folders labelled Pos and Neg
     os.mkdir("./data/train/pos")
     os.mkdir("./data/train/neg")
-    os.mkdir("./data/val")
-    os.mkdir("./data/val/pos")
-    os.mkdir("./data/val/neg")
-    os.mkdir("./data/test")
-    os.mkdir("./data/test/pos")
-    os.mkdir("./data/test/neg")
-    # Get validation index and test index
-
     # Add files based on label
-    for dirname, _, filenames in os.walk(os.getcwd() + "./data/train/"):
-        val_idx= floor(len(filenames) - len(filenames) * (val_split + test_split))
-        test_idx = floor(len(filenames) - len(filenames) * test_split)
+    for _, _, filenames in os.walk(os.getcwd() + "./data/train/"):
         # Iterate through each image in list with its subsequent txt
         for i in range(0,len(filenames) - 1,2):
             img_name = filenames[i]
@@ -33,11 +22,19 @@ def order_dataset(val_split: float = 0.2, test_split: float = 0.1):
             label = int(file.read(1))
             file.close()
             # Move image to folder pos or neg or 1 depending on label
-            move_to = "./data/train/" if i < val_idx else "./data/val/" if i < test_idx else "./data/test/"
             if label == 1:
-                shutil.move(f"./data/train/{img_name}", move_to + "pos/")
+                moves.append((f"./data/train/{img_name}", "./data/train/pos/"))
             else:
-                shutil.move(f"./data/train/{img_name}", move_to + "neg/")
+                moves.append((f"./data/train/{img_name}", "./data/train/neg/"))
+            removes.append(loc_txt_dir)
             # Delete text file
-            os.remove(loc_txt_dir)
-    
+    # Classify all image files
+    for mv in moves:
+        key = mv[0]
+        dest = mv[1]
+        shutil.move(key,dest)
+    # Remove all .txt files
+    for rm in removes:
+        os.remove(rm)
+
+order_dataset()
